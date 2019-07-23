a2929f0a593933a268c2c02c0b8dd4e15e89c957
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-07-23 23:58:45 +1000

    x

---
+++ Top-Down Shooter/Top Down Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //General:
+    public float xAxis;
+    public float yAxis;
+    public float speed = 2f;
+
+    //Reference:
+    public Rigidbody2D rigid;
+
+    public void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+    }
+
+    public void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(xAxis * speed, yAxis * speed);
+    }
+}
