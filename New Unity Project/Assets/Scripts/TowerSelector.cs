6662d559ae6383785981d6dae2d8ed14110a635e
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-07-24 12:03:08 +1000

    x

---
+++ New Unity Project/Assets/Scripts/TowerSelector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSelector : MonoBehaviour
+{
+    public int towerMax = 2;
+    public int towerIndex = 1;
+
+    public void SpawnTower(Vector3 position)
+    {   
+        GameObject towerPrefab = Resources.Load<GameObject>("Prefabs/Towers/Tower " + towerIndex);
+        Instantiate(towerPrefab, position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        if (towerIndex > towerMax)
+        {
+            towerIndex = 1;
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                towerIndex++;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(mouseRay, out hit))
+            {
+                Placeable p = hit.collider.GetComponent<Placeable>();
+                if (p && p.isOccupied == false)
+                {
+                    SpawnTower(hit.transform.position);
+                    p.isOccupied = true;
+                }
+            }
+        }
+    }
+}
