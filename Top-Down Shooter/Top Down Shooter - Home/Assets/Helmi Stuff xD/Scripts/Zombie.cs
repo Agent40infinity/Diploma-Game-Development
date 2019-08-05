882634d114f460f6aebfb24867fd8199f7453478
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-08-06 00:19:04 +1000

    x

---
+++ Top-Down Shooter/Top Down Shooter - Home/Assets/Helmi Stuff xD/Scripts/Zombie.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public float stoppingDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            speed = 0;  
+        }
+
+    }
+}
