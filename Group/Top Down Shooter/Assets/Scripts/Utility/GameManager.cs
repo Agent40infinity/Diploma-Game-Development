6ee343a3d78a8f304957e251950fcb430b162b72
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-08-09 21:00:36 +1000

    x

--- Group/Top Down Shooter/Assets/Scripts/Utility/GameManager.cs
+++ Group/Top Down Shooter/Assets/Scripts/Utility/GameManager.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Wall;
+    public int tileWidth = 16;
+    public int tileHeight = 9;
+    public int[,] tilesetPreset1 =
+        {
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    };
+    public int[,] tilesetPreset2 =
+        {
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    };
+    public int[,] tilesetPreset3 =
+        {
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,1,1,1,0,0,0,0,1,1,1,1,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1},
+        {1,0,1,1,1,1,0,0,0,0,1,1,1,1,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    };
+
+    public void Start()
     {
-        
+        Wall = Resources.Load<GameObject>("Prefabs/Wall");
+        Tilesets();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Tilesets()
     {
-        
+        for (int y = 0; y < tileHeight; y++) //Checks the  Y position and value within the array to set it's content.
+        {
+            for (int x = 0; x < tileWidth; x++) //Checks the  X position and value within the array to set it's content.
+            {  
+                switch (tilesetPreset1[y, x]) //Generation for Tileset PReset 1
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        Instantiate(Wall, new Vector3(x, y, 0), transform.rotation);
+                        break;
+                }
+                switch (tilesetPreset2[y, x]) //Generation for Tileset PReset 2
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        Instantiate(Wall, new Vector3(x, y, 0), transform.rotation);
+                        break;
+                }
+                switch (tilesetPreset3[y, x]) //Generation for Tileset Preset 3
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        Instantiate(Wall, new Vector3(x, y, 0), transform.rotation);
+                        break;
+                }
+            }
+        }
     }
 }
