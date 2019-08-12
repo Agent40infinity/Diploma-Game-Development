198f9a1e6067138c750740787a282a4b9f324f79
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-08-12 11:58:18 +1000

    x

---
+++ Jaymie/Login Screen/Assets/Scripts/Login.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Login : MonoBehaviour
+{
+    IEnumerator CreateUser(string username, string email, string password)
+    {
+        string createUserURL = "hhtp://localhost/nsirpg/insertuser.php";
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("email", email);
+        form.AddField("password", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(createUserURL, form);
+        yeild return webRequest.Send();
+    }
+
+
+}
