2f62b7f6fa82b77dfb8caa697a7cc78b430bf043
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-08-19 14:38:34 +1000

    x

--- Group/Top Down Shooter/Assets/Scripts/Utility/GameManager.cs
+++ Group/Top Down Shooter/Assets/Scripts/Utility/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     //General:
     public float xOffset;
     public float yOffset;
+    public int random;
 
     //Reference:
     public GameObject player;
@@ -18,6 +19,7 @@ public class GameManager : MonoBehaviour
     {
         player = GameObject.Find("Player");
         spawnRoom = GameObject.Find("Spawn");
+        curRoom = spawnRoom;
         for (int i = 0; i < roomPresets.Length; i++)
         {
             roomPresets[i] = Resources.Load<GameObject>("Prefabs/Presets/Preset " + (i + 1));
@@ -27,25 +29,24 @@ public class GameManager : MonoBehaviour
 
     public void Update()
     {
-
-
         if (curRoom.GetComponent<Room>().beenVisited == 1)
         {
             for (int i = 0; i < curRoom.GetComponent<Room>().doors.Length; i++)
             {
                 if (curRoom.GetComponent<Room>().doors[0])
-                { xOffset = 0f; yOffset = -20f; }
+                { xOffset = 0f; yOffset = -16.5f; }
                 else if (curRoom.GetComponent<Room>().doors[1])
-                { xOffset = -20f; yOffset = 0f; }
+                { xOffset = -16.5f; yOffset = 0f; }
                 else if (curRoom.GetComponent<Room>().doors[2])
-                { xOffset = 0f; yOffset = 20f; }
+                { xOffset = 0f; yOffset = 16.5f; }
                 else if (curRoom.GetComponent<Room>().doors[3])
-                { xOffset = 20f; yOffset = 0; }
+                { xOffset = 16.5f; yOffset = 0; }
 
-                Instantiate(roomPresets[Random.Range(0, 4)], new Vector2(curRoom.transform.position.x + xOffset, curRoom.transform.position.y + yOffset), Quaternion.identity);
-                curRoom.GetComponent<Room>().beenVisited = 2;
+                random = Random.Range(-1, 4);
+                Instantiate(roomPresets[random], new Vector2(curRoom.transform.position.x + xOffset, curRoom.transform.position.y + yOffset), Quaternion.identity);
+                Debug.Log(random);
             }
-            
+            curRoom.GetComponent<Room>().beenVisited = 2;
         }
     }
 }
--- Group/Top Down Shooter/Assets/Scripts/Utility/Room.cs
+++ Group/Top Down Shooter/Assets/Scripts/Utility/Room.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public int beenVisited = 0;
-    public Transform[] doors;
+    public Transform[] doors = new Transform[4];
+    public GameObject curRoom = null;
 
     public void Start()
     {
-        for (int i = 0; i < doors.Length; i++)
-        {
-            doors[i] = GetComponentInChildren<Transform>();
-        }
+        //for (int i = 0; i < doors.Length; i++)
+        //{
+        //    doors[i] = GetComponentInChildren<Transform>();
+        //}
     }
 
     public void OnTriggerStay2D(Collider2D other)
@@ -20,8 +21,7 @@ public class Room : MonoBehaviour
         if (other.tag == "Player")
         {
             beenVisited = 1;
-            GetComponent<GameManager>().curRoom = gameObject;
-            Debug.Log(GetComponent<GameManager>().curRoom);
+            GameObject.Find("GameManager").GetComponent<GameManager>().curRoom = gameObject;
         }
     }
 }
