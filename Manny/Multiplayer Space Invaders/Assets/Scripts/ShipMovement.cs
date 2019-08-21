1956fff76d4e89892fe1dee8e493105bd7b6b1c9
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-08-21 14:10:59 +1000

    x

---
+++ Manny/Multiplayer Space Invaders/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ShipMovement : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private void FixedUpdate()
+    {
+        if (isLocalPlayer)
+        {
+            float movement = Input.GetAxis("Horizontal");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed, 0f);
+        }
+    }
+}
