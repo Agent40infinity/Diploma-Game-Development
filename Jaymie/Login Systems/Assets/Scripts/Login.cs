36883127e467123d8bd3961c9126fd5cd3f8f0d7
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-08-26 23:55:34 +1000

    x

---
+++ Jaymie/Login Systems/Assets/Scripts/Login.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class Login : MonoBehaviour
+{
+    #region Variables
+    public InputField usernameInput;
+    public InputField emailInput;
+    public InputField passwordInput;
+
+    public InputField usernameInputLogin;
+    public InputField passwordInputLogin;
+
+    private string characters = "0123456789abcdefghijklmnopqrstuvwxABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private string code = "";
+    #endregion
+
+    #region Create User
+    IEnumerator CreateUser(string username, string email, string password)
+    {
+        string createUserURL = "http://localhost/nsirpg/InsertUser.php";
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("email", email);
+        form.AddField("password", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(createUserURL, form);
+        yield return webRequest.SendWebRequest();
+        Debug.Log(webRequest);
+    }
+
+    public void CreateNewUser()
+    {
+        StartCoroutine(CreateUser(usernameInput.text, emailInput.text, passwordInput.text));
+    }
+    #endregion
+
+    #region Login
+    IEnumerator UserLogin(string username, string password)
+    {
+        string createUserURL = "http://localhost/nsirpg/UserLogin.php";
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("password", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(createUserURL, form);
+        yield return webRequest.SendWebRequest();
+        Debug.Log(webRequest.downloadHandler.text);
+        if (webRequest.downloadHandler.text == "Logged In")
+        {
+            SceneManager.LoadScene(1);
+        }
+        else { }
+    }
+
+    public void LoginUser()
+    {
+        StartCoroutine(UserLogin(usernameInputLogin.text, passwordInputLogin.text));
+    }
+    #endregion
+
+    #region Send Email
+    void SendEmail(string _email, string _username)
+    {
+        CreateCode();
+        MailMessage mail = new MailMessage();
+        mail.From = new MailAddress("sqlunityclasssydney@gmail.com");
+        mail.To.Add(_email);
+        mail.Subject = "NSIRPG Password Reset";
+        mail.Body = "Hello " + _username + "\nReset using this code: " + code;
+
+        //connect to google
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        //be able to send through ports
+        smtpServer.Port = 25; //80 25 
+        //login to google
+        smtpServer.Credentials = new NetworkCredential("sqlunityclasssydney@gmail.com", "sqlpassword") as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate cert, X509Chain chain, SslPolicyErrors policyErrors)
+        { return true; };
+        //send message
+        smtpServer.Send(mail);
+        Debug.Log("Sending Email");
+    }
+
+    void CreateCode()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            int a = UnityEngine.Random.Range(0, characters.Length);
+            code = code + characters[a];
+        }
+
+        Debug.Log(code);
+    }
+    #endregion
+
+    //#region forgot User
+    //public void SubmitLogin()
+    //{
+    //    StartCoroutine(UserLogin(username, password));
+    //}
+
+    //IEnumerator ForgotUser(string email)
+    //{
+    //    string forgotURL = "http://localhost/nsirpg/checkemail.php";
+    //    WWWForm form = new WWWForm();
+    //    form.AddField("email_Post", email);
+    //    UnityWebRequest webRequest = UnityWebRequest.Post(forgotURL, form);
+    //    yield return webRequest.SendWebRequest();
+    //    Debug.Log(webRequest.downloadHandler.text);
+    //    if (webRequest.downloadHandler.text == "User Not Found")
+    //    {
+    //        createAccToolTip = webRequest.downloadHandler.text;
+    //    }
+    //    else
+    //    {
+    //        SendEmail(email, username);
+    //    }
+    //}
+    //#endregion
+}
