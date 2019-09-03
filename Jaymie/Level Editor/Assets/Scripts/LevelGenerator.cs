c31d154d9c79e454642efbc086cddc79ff47d1d3
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-09-03 12:57:03 +1000

    x

---
+++ Jaymie/Level Editor/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D mapTexture;
+    public PixelToObject[] pixelColorMappings;
+    private Color pixelColor;
+
+    public void Start()
+    {
+        GenerateLevel();
+    }
+
+    public void GenerateLevel()
+    {
+        for (int i = 0; i < mapTexture.width; i++)
+        {
+            for (int j = 0; j < mapTexture.height; j++)
+            {
+                GenerateObject(i, j);
+            }
+        }
+    }
+
+    public void GenerateObject(int x, int y)
+    {
+        pixelColor = mapTexture.GetPixel(x, y);
+        if (pixelColor.a == 0)
+        {
+            Debug.Log("Mhm");
+            return;            
+        }
+
+        foreach (PixelToObject pixelColorMapping in pixelColorMappings)
+        {
+            if (pixelColorMapping.pixelColor.Equals(pixelColor))
+            {
+                Vector2 position = new Vector2(x, y);
+                Instantiate(pixelColorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
---
+++ Jaymie/Level Editor/Assets/Scripts/PixelToObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+[System.Serializable]
+
+public class PixelToObject 
+{
+    public Color pixelColor;
+    public GameObject prefab;
+}
