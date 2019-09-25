6be0ff6b82f56f05e65c92edbb6c97a4342300ca
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-09-25 13:51:56 +1000

    x

---
+++ Group/Persisting Objects/Assets/Scripts/Fabricating Shapes/Shape.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : PersistableObject
+{
+    int shapeId = int.MinValue;
+    Color color;
+    MeshRenderer meshRenderer;
+    static int colorPropertyId = Shader.PropertyToID("_Color");
+    static MaterialPropertyBlock sharedPropertyBlock;
+
+    public int ShapeId
+    {
+        get { return shapeId; }
+        set
+        {
+            if (shapeId == int.MinValue && value != int.MinValue)
+            {
+                shapeId = value;
+            }
+            else
+            {
+                Debug.LogError("Not allowed to change shapeId.");
+            }
+        }
+    }
+
+    public int MaterialId { get; private set; }
+
+    public void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void SetMaterial(Material material, int materialId)
+    {
+        meshRenderer.material = material;
+        MaterialId = materialId;
+    }
+
+    public void SetColor(Color color)
+    {
+        this.color = color;
+        if (sharedPropertyBlock == null)
+        {
+            sharedPropertyBlock = new MaterialPropertyBlock();
+        }
+        sharedPropertyBlock.SetColor(colorPropertyId, color);
+        meshRenderer.SetPropertyBlock(sharedPropertyBlock);
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        base.Save(writer);
+        writer.Write(color);
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        base.Load(reader);
+        SetColor(reader.Version > 0 ? reader.ReadColor() : Color.white);
+    }
+}
