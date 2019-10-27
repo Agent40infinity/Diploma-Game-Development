17934c61fbde777e8cc8eff4d3ab3c77111d48db
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-10-28 10:16:07 +1100

    x

---
+++ Manny/Persisting Objects/Assets/Scripts/Level Variety/GameLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLevel : MonoBehaviour
+{
+    [SerializeField]
+    SpawnZone spawnZone;
+
+    public void Start()
+    {
+        Game.Instance.SpawnZoneOfLevel = spawnZone;
+    }
+}
---
+++ Manny/Persisting Objects/Assets/Scripts/Level Variety/SpawnZone.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpawnZone : MonoBehaviour
+{
+    public abstract Vector3 SpawnPoint { get; }
+}
